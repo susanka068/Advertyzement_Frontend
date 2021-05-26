d646509267e61afbc7222eb9ade8ebe97c179c7a
susanka majumder <40341077+susanka068@users.noreply.github.com>
2021-05-26 15:25:27 +0000

    spinner added in case of lazy loading

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 import Header from './components/Header'
 import UserGrid from './components/UserGrid'
+import Spinner from './Fidget-spinner.gif'
 
 import axios from 'axios' ; 
 
@@ -13,20 +14,23 @@ function App() {
   const [users , setUsers ]  = useState([]) ; 
   const [isLoading , setIsLoading ] = useState(1) ; // isLoading = 1 -> means loading isn't initited , isLoading = 2 -> means loading is initited but not finished , isLoading = 3 -> means loading is finished .
 
+
  
       const fetchUsers = async () => {
           const result = await axios(`https://reqres.in/api/users?page=1`) ;
           console.log(result.data);
           setUsers(result.data.data) ; 
-          setIsLoading(false) ;
+          setIsLoading(3) ;
       }
 
 
   return (
     <div className="App" >
-        <Header fetchUsers={fetchUsers} />
-      <div className="container" >        
-        <UserGrid users={users} />
+        <Header fetchUsers={fetchUsers} setIsLoading={setIsLoading} />
+      <div className="container" >
+        {
+          (isLoading==2)?<img src={Spinner} alt={"loading..."} style={{ "width" :"80px" , margin:"auto" , display:"block" }} />:<UserGrid users={users} />
+        }        
       </div>
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,14 @@ import React from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Button from 'react-bootstrap/Button'
 
-export default function Header( { fetchUsers }) {
+export default function Header( { fetchUsers ,  setIsLoading }) {
     return (
         <div>
             <Navbar bg="primary" variant="dark" >
                 <Navbar.Brand style={{ fontSize: 30 } }>
                     susanka068
                 </Navbar.Brand>
-                <Button onClick={fetchUsers} variant="outline-light">Get Users</Button>
+                <Button onClick={() => { setIsLoading(2) ;  fetchUsers() ; }} variant="outline-light">Get Users</Button>
             </Navbar>
         </div>
     )
