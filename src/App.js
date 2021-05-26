ac99092bfe9e4aa686738c87acfac6517f123ff6
susanka majumder <40341077+susanka068@users.noreply.github.com>
2021-05-26 15:07:48 +0000

    get user button functionality added

--- src/App.js
+++ src/App.js
@@ -11,24 +11,21 @@ import './App.css' ;
 function App() {
 
   const [users , setUsers ]  = useState([]) ; 
-  const [isLoading , setIsLoading ] = useState(true) ; 
+  const [isLoading , setIsLoading ] = useState(1) ; // isLoading = 1 -> means loading isn't initited , isLoading = 2 -> means loading is initited but not finished , isLoading = 3 -> means loading is finished .
 
-  useEffect( () => {
+ 
       const fetchUsers = async () => {
           const result = await axios(`https://reqres.in/api/users?page=1`) ;
           console.log(result.data);
           setUsers(result.data.data) ; 
           setIsLoading(false) ;
       }
-      fetchUsers();
-  } , [] ) 
 
 
   return (
     <div className="App" >
-        <Header />
-      <div className="container" >
-        
+        <Header fetchUsers={fetchUsers} />
+      <div className="container" >        
         <UserGrid users={users} />
       </div>
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,14 @@ import React from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Button from 'react-bootstrap/Button'
 
-export default function Header() {
+export default function Header( { fetchUsers }) {
     return (
         <div>
             <Navbar bg="primary" variant="dark" >
                 <Navbar.Brand style={{ fontSize: 30 } }>
                     susanka068
                 </Navbar.Brand>
-                <Button variant="outline-light">Get Users</Button>
+                <Button onClick={fetchUsers} variant="outline-light">Get Users</Button>
             </Navbar>
         </div>
     )
